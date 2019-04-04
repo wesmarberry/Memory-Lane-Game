a0439dee9ead7b22c064b84598591ed68ecbb3d2
Wes <wesmarberry@gmail.com>
2019-04-04 17:06:39 -0500

    created working generate board method on game object

--- js/app.js
+++ js/app.js
@@ -18,22 +18,35 @@ class Player {
 	}
 }
 
+
 const game = {
 	numberPlayers: 0,
 	activeSquares: [],
-	boardSize: 3,
-	generateBoard(){
-
-	}
+	boardSize: 9,
+	divSize: 50,
+	boardContainerSize: (this.boardSize * this.divSize),
+	letters: [],
+	generateBoard(num){
+		$('#game-board').append($('<div/>').attr('id', 'end-platform').css('height', this.divSize + 'px').css('width', this.divSize + 'px').css('border', '1px solid black'))
+		$('#game-board').append($('<div/>').attr('id', 'main-grid').css('height', this.boardSize * this.divSize + (this.boardSize * 2) + 'px').css('width', this.boardSize * this.divSize + (this.boardSize *2) + 'px'))
+		$('#game-board').append($('<div/>').attr('id', 'start-platform').css('height', this.divSize + 'px').css('width', this.divSize + 'px').css('border', '1px solid black'))
+		for (let i = 0; i < this.boardSize; i++) {
+			const row = i
+			for (let j = 0; j < this.boardSize; j++) {
+				const column = j
+				$('#main-grid').prepend($('<div/>').attr('id', row + '-' + column).css('height', this.divSize + 'px').css('width', this.divSize + 'px').css('border', '1px solid black'))
+			}
+		}
+	},
 	randomPath(){
 
-	}
+	},
 	showPath(){
 
-	}
+	},
 	restart(){
-		
+
 	}
 }
 
-
+console.log(game.boardContainerSize);
