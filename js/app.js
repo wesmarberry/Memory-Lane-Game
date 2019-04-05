a808c7fef6564b6f118a668ad0f367926c0373a4
Wes <wesmarberry@gmail.com>
2019-04-04 22:59:30 -0500

    Created working randomly generated path for any board size

--- js/app.js
+++ js/app.js
@@ -18,35 +18,124 @@ class Player {
 	}
 }
 
+// class Square {
+// 	constructor() {
+// 		this.height = '50px'
+// 		this.width = '50px'
+
+// 	}
+// }
+
 
 const game = {
 	numberPlayers: 0,
+	squares: [],
 	activeSquares: [],
-	boardSize: 9,
+	boardSize: 7,
 	divSize: 50,
 	boardContainerSize: (this.boardSize * this.divSize),
-	letters: [],
+	availableSquareArr: [],
+	startSquare: '',
+	endSquare: '',
+	prevSquare: '',
+	currentSquare: '',
 	generateBoard(num){
 		$('#game-board').append($('<div/>').attr('id', 'end-platform').css('height', this.divSize + 'px').css('width', this.divSize + 'px').css('border', '1px solid black'))
 		$('#game-board').append($('<div/>').attr('id', 'main-grid').css('height', this.boardSize * this.divSize + (this.boardSize * 2) + 'px').css('width', this.boardSize * this.divSize + (this.boardSize *2) + 'px'))
 		$('#game-board').append($('<div/>').attr('id', 'start-platform').css('height', this.divSize + 'px').css('width', this.divSize + 'px').css('border', '1px solid black'))
-		for (let i = 0; i < this.boardSize; i++) {
+		for (let i = 1; i <= this.boardSize; i++) {
 			const row = i
-			for (let j = 0; j < this.boardSize; j++) {
+			for (let j = 1; j <= this.boardSize; j++) {
 				const column = j
 				$('#main-grid').prepend($('<div/>').attr('id', row + '-' + column).css('height', this.divSize + 'px').css('width', this.divSize + 'px').css('border', '1px solid black'))
 			}
 		}
 	},
+	setStartSquare(){
+		const row = 1
+		const column = Math.ceil(this.boardSize / 2)
+		const startSquareId = row + '-' + column
+		this.startSquare = startSquareId
+		console.log(startSquareId);
+		this.endSquare = this.boardSize + '-' + column
+	},
+	availableSquares(squareId){
+		const row = Number(squareId[0])
+		const column = Number(squareId[2])
+		let square1;
+		let square2;
+		let square3;
+		if (row === 1 && column === 1) {
+			square1 = (row + 1) + '-' + column 
+			this.availableSquareArr.push(square1)
+		} else if (row === this.boardSize && column ===1) {
+			square2 = row + '-' + (column + 1)
+			this.availableSquareArr.push(square2)
+		} else if (row === this.boardSize && column === this.boardSize) {
+			square1 = row + '-' + (column - 1)
+			this.availableSquareArr.push(square1)
+		} else if (row === 1 && column === this.boardSize) {
+			square1 = (row + 1) + '-' + column
+			this.availableSquareArr.push(square1)
+		} else if (row === this.boardSize && column < this.endSquare[2]) {
+			square1 = row + '-' + (column + 1)
+			this.availableSquareArr.push(square1)
+		} else if (row === this.boardSize && column > this.endSquare[2]) {
+			square1 = row + '-' + (column - 1)
+			this.availableSquareArr.push(square1)
+		} else if (column === 1) {
+			square1 = (row + 1) + '-' + column
+			square2 = row + '-' + (column + 1)
+			this.availableSquareArr.push(square1)
+			this.availableSquareArr.push(square2)
+		} else if (column === this.boardSize) {
+			square1 = (row + 1) + '-' + column
+			square2 = row + '-' + (column - 1)
+			this.availableSquareArr.push(square1)
+			this.availableSquareArr.push(square2)
+		} else {
+			square1 = (row + 1) + '-' + column
+			square2 = row + '-' + (column - 1)
+			square3 = row + '-' + (column + 1)
+			this.availableSquareArr.push(square1)
+			this.availableSquareArr.push(square2)
+			this.availableSquareArr.push(square3)
+		}
+		console.log(this.availableSquareArr);
+	},
 	randomPath(){
-
+		this.setStartSquare()
+		this.currentSquare = this.startSquare
+		this.activeSquares.push(this.currentSquare)
+		console.log(this.currentSquare);
+		console.log(this.endSquare);
+ 		while (this.currentSquare !== this.endSquare) {
+	 			this.availableSquares(this.currentSquare)
+	 			console.log(this.availableSquareArr);
+	 			let rand = Math.floor(Math.random() * this.availableSquareArr.length)
+	 			if (this.activeSquares.includes(this.availableSquareArr[rand]) === false) {
+		 			this.activeSquares.push(this.availableSquareArr[rand])
+		 			this.currentSquare = this.availableSquareArr[rand]
+		 			this.availableSquareArr = []
+		 			console.log(this.currentSquare);
+		 			console.log(this.activeSquares);
+	 			} else {
+	 				this.currentSquare = this.currentSquare
+	 			}
+ 		}
+ 		// this.activeSquares.shift(this.startSquare)
+ 		console.log('Final active squares ' + this.activeSquares);
 	},
 	showPath(){
-
+		for (let i = 0; i < this.activeSquares.length; i++) {
+			$('#' + this.activeSquares[i]).attr('class', 'active')
+		}
 	},
 	restart(){
 
 	}
 }
 
-console.log(game.boardContainerSize);
+game.generateBoard()
+game.randomPath()
+game.showPath()
